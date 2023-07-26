01318d99fe22a6aea008b1d10c6d2d30b1360020
Greed1n <108575314+shok111@users.noreply.github.com>
2023-07-26 21:51:25 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,9 @@
+document.addEventListener("click", function (e) {
+	const targetElement = e.target;
+
+	if (targetElement.closest('.menu-icon')) {
+		document.documentElement.classList.toggle('menu-open');
+		e.preventDefault();
+	}
+
+})
